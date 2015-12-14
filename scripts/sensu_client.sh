e6424757f97141b2bfa36ba84937347c135def3b
Gilad Judes <gilad.judes@teamaol.com>
2015-12-14 11:10:09 +0200

    sensu configurations

--- scripts/sensu_client.sh
+++ scripts/sensu_client.sh
@@ -6,3 +6,7 @@ apt-get update
 apt-get install sensu
 mkdir -p /etc/sensu/ssl
 cp /tmp/ssl/client/*.pem /etc/sensu/ssl
+cp /tmp/sensu/rabbitmq.json /etc/sensu/conf.d/rabbitmq.json
+cp /tmp/sensu/client.json /etc/sensu/conf.d/client.json
+update-rc.d sensu-client defaults
+/etc/init.d/sensu-client start
--- scripts/sensu_server.sh
+++ scripts/sensu_server.sh
@@ -4,4 +4,17 @@ echo "deb     http://repos.sensuapp.org/apt sensu main" > /etc/apt/sources.list.
 apt-get update
 apt-get install sensu
 mkdir -p /etc/sensu/ssl
-cp /tmp/ssl/server/.pem /etc/sensu/ssl
+cp /tmp/ssl/server/*.pem /etc/sensu/ssl
+cp /tmp/sensu/rabbitmq.json /etc/sensu/conf.d/rabbitmq.json
+cp /tmp/server/redis.json /etc/sensu/conf.d/redis.json
+cp /tmp/server/api.json /etc/sensu/conf.d/api.json
+cp /tmp/server/dashboard.json /etc/sensu/conf.d/dashboard.json
+cp /tmp/sensu/client.json /etc/sensu/conf.d/client.json
+update-rc.d sensu-server defaults
+update-rc.d sensu-client defaults
+update-rc.d sensu-api defaults
+update-rc.d sensu-dashboard defaults
+/etc/init.d/sensu-server start
+/etc/init.d/sensu-client start
+/etc/init.d/sensu-api start
+/etc/init.d/sensu-dashboard start
